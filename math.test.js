a671d162de04e6d0b02277fabd2f8aa610080811
boom-punsin <121000491+boom-punsin@users.noreply.github.com>
2024-06-28 13:15:07 +0700

    Update math.test.js

--- math.test.js
+++ math.test.js
@@ -1,11 +1,11 @@
-const sum = require('./sum');
+const {sum,multipy,devide}= require('./math');
 
 test('Adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3)
  })
 
 test('Multipy 2 * 3 to equal 6', () => {
-    expect(multipy(2,2)).toBe(6)
+    expect(multipy(2,3)).toBe(6)
  })
 
  test('Devide 10 + 2 to equal 5', () => {
