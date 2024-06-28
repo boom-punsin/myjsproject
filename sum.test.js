2fdd3abbda60b51f1dff69302dd9a619ab5c873f
boom-punsin <121000491+boom-punsin@users.noreply.github.com>
2024-06-28 12:09:31 +0700

    save

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,11 @@ const sum = require('./sum');
 test('Adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3)
  })
+
+test('Adds 1 + 2 to equal 3', () => {
+    expect(sum(2,2)).toBe(4)
+ })
+
+test('Adds 1 + 2 to equal 3', () => {
+    expect(sum(2,5)).toBe(4)
+ })
--- sum.test_4.js
+++
@@ -1,5 +0,0 @@
-const sum = require('./sum');
-
-test('Adds 1 + 2 to equal 3', () => {
-    expect(sum(7,3)).toBe(10)
- })
